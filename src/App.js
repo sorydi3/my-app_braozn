7c087533da83a2a0062c3ff83ca547fdbfb96b30
ibradi3 <ibrahimasory22@hotmail.com>
2022-12-04 23:16:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-white-200 ">
+        <div className="container mx-auto h-screen bg-orange-100">
+
+           
+            
+        </div>
+    </div>
+  );
+}
+
+export default App;
